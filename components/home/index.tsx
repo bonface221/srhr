bb6857da857da56aced252d09ee2ac5d5c185e54
bonface221 <mainaboniface221@gmail.com>
2024-05-02 17:14:14 +0300

    merge conflicts

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,40 +1,13 @@
-<<<<<<< HEAD
-import ABitAboutUs from "./abit-about-us";
-import OurMission from "./our-mission-vision";
-import OurServices from "./our-services";
-=======
->>>>>>> eef776693366567900442d37b87efc1de12f4777
 import dynamic from "next/dynamic";
 
+export { default as LatestNews } from "../common/latest-news";
 export { default as ABitAboutUs } from "./abit-about-us";
 export { default as Hero } from "./hero";
 export { default as OurMission } from "./our-mission-vision";
 export { default as OurServices } from "./our-services";
-export { default as LatestNews } from "../common/latest-news";
 export { default as Stats } from "./stats";
 
 export const SRHRAllianceOnYoutube = dynamic(
   () => import("./srhr-alliance-on-youtube"),
   { ssr: false }
 );
-<<<<<<< HEAD
-
-const Hero = dynamic(() => import("./hero"));
-
-const Landing = () => {
-  return (
-    <>
-      <Hero />
-      <Stats />
-      <ABitAboutUs />
-      <OurMission />
-      <OurServices />
-      <SRHRAllianceOnYoutube />
-      <LatestNews />
-    </>
-  );
-};
-
-export default Landing;
-=======
->>>>>>> eef776693366567900442d37b87efc1de12f4777
