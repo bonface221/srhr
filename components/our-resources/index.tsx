fcf60dfb04780fc5072746e2fa9fe43d90cd0d0d
bonface221 <mainaboniface221@gmail.com>
2024-05-30 23:18:46 +0300

    fix broken links

--- components/our-resources/index.tsx
+++ components/our-resources/index.tsx
@@ -7,6 +7,7 @@ import {
   SimpleGrid,
   Stack,
   Text,
+  Tooltip,
 } from "@chakra-ui/react";
 import Image from "next/image";
 import React from "react";
@@ -35,30 +36,33 @@ const OurResources = () => {
         </Box>
         <Stack justify="center" gap={3}>
           <Heading as="h3" fontSize="3xl">
-            Lorem ipsum dolor sit amet, consectetur adipisicing.
+            2023 ANNUAL REPORT SRHR Alliance
           </Heading>
           <Text>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
-            sapiente perferendis provident sed labore nemo nisi. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Facere sequi molestias
-            voluptatibus alias ipsa non obcaecati incidunt iure! Lorem ipsum,
-            dolor sit amet consectetur adipisicing elit. Mollitia adipisci,
-            voluptatum iusto est et ad optio, eos unde soluta minima
-            voluptatibus officia fuga, rerum ipsam magnam praesentium eaque quia
-            deleniti.
+            2023 ANNUAL REPORT SRHR Alliance is a comprehensive report that
+            details the activities and achievements of the SRHR Alliance in the
+            year 2023.
           </Text>
-          <Button
-            variant="outline"
-            w="fit-content"
-            borderColor="brand.red"
-            borderRadius="20px"
-            color="brand.red"
+          <Tooltip
+            label="Download button disabled for now"
+            aria-label="Button disabled"
+            hasArrow
+            bg="brand.red"
           >
-            <Flex align="center" gap={2}>
-              <Text>Download Pdf</Text>
-              <IoCloudDownloadOutline size={20} />
-            </Flex>
-          </Button>
+            <Button
+              variant="outline"
+              w="fit-content"
+              borderColor="brand.red"
+              borderRadius="20px"
+              color="brand.red"
+              isDisabled
+            >
+              <Flex align="center" gap={2}>
+                <Text>Download Pdf</Text>
+                <IoCloudDownloadOutline size={20} />
+              </Flex>
+            </Button>
+          </Tooltip>
         </Stack>
       </SimpleGrid>
     </Stack>
